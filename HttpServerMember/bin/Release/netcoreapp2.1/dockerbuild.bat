880353d74d7e9729c11d6b0e282bb8e647a721f9
frank12001 <gihhgihh11@gmail.com>
2019-04-13 01:18:35 +0800

    檔案結構調整 : 刪除既有檔案

--- HttpServerMember/bin/Release/netcoreapp2.1/dockerbuild.bat
+++
@@ -1,3 +0,0 @@
-docker build -t httpserver .
-docker tag httpserver registry-intl.cn-shanghai.aliyuncs.com/slither/agent:httpservertemp
-docker push registry-intl.cn-shanghai.aliyuncs.com/slither/agent:httpservertemp
--- SocketServerDemo1/bin/Release/netcoreapp2.1/publish/dockerbuild.bat
+++
@@ -1,3 +0,0 @@
-docker build -t httpserver .
-docker tag httpserver registry-intl.cn-shanghai.aliyuncs.com/slither/agent:rudpservertemp
-docker push registry-intl.cn-shanghai.aliyuncs.com/slither/agent:rudpservertemp
