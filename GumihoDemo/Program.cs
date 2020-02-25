60e141b84832c5e34d6fe1674cbec7fa17d7f0ec
Costa Chen <developerjoy@gmail.com>
2020-02-25 15:11:49 +0800

    change output path.

---
+++ GumihoDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GumihoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
